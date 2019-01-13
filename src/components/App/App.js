5794a0897f8690ba41d0920045027c28afaf2175
rmrotek <remek87@gmail.com>
2019-01-13 15:12:48 +0100

    basic routing added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
 import { Grid, Typography } from '@material-ui/core'
-import UserList from './UserList';
 import UserListView from './UserListView';
+
+import UserView from './UserView';
 
 class App extends Component {
   render() {
     return (
-      <UserListView />
+
+      <Router>
+        <Grid container justify='center' >
+          <Route exact path="/" component={UserListView} />
+          <Route exact path="/user" component={UserView} />
+        </Grid>
+
+
+      </Router>
     );
   }
 }
---
+++ src/components/App/UserView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const UserView = ({ user }) => (
+
+   <>
+    userview
+   </>
+
+);
+
+export default UserView;
