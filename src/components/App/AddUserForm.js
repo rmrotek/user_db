d8d85da59465168a1934c1452b34a7d7a214ad4e
rmrotek <remek87@gmail.com>
2019-01-14 16:07:01 +0100

    adduserform thinned out

--- src/components/App/AddUserForm.js
+++ src/components/App/AddUserForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid, Typography, Button, TextField } from '@material-ui/core';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 
 import { connect } from 'react-redux'
 import { requestUserAdd } from "../../store/reducers/users"
@@ -27,7 +27,7 @@ let AddUserForm = ({ history, requestUserAdd }) => {
         if (!values.username) {
           errors.username = 'Required';
         }
-        
+
         return errors;
       }}
       onSubmit={(values, { setSubmitting }) => {
@@ -37,92 +37,68 @@ let AddUserForm = ({ history, requestUserAdd }) => {
         }, 400);
       }}
     >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleSubmit,
-        isSubmitting,
-        handleBlur
-      }) => (
-          <form onSubmit={handleSubmit}>
-            <Grid container justify='center'>
+      {({ isSubmitting }) => (
+        <Form >
+          <Grid container justify='center'>
 
-              <Grid container justify='center' item xl={11} lg={11} md={11} sm={11} xs={11} spacing={8} style={{ background: 'lightgrey' }}>
-                <Grid item xl={11} lg={11} md={11} sm={11} xs={11}>
-                  <Typography variant='h4' align='center' >
-                    {'Add a User to Database'}
-                  </Typography>
+            <Grid container justify='center' item xl={11} lg={11} md={11} sm={11} xs={11} spacing={8} style={{ background: 'lightgrey' }}>
+              <Grid item xl={11} lg={11} md={11} sm={11} xs={11}>
+                <Typography variant='h4' align='center' >
+                  {'Add a User to Database'}
+                </Typography>
+              </Grid>
+              <Grid item xl={6} lg={12} md={12} sm={11} xs={11} >
 
-                </Grid>
-                <Grid item xl={6} lg={12} md={12} sm={11} xs={11} >
+                <Typography variant='h6' align='center' >
+                  <Field
+                    component={TextField}
+                    type="text"
+                    name="name"
+                    label='name'
+                  />
 
-                  <Typography variant='h6' align='center' >
-                    <TextField
-                      error={errors.name && touched.name && true}
-                      type="text"
-                      name="name"
-                      label='name'
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.name}
-                    />
-                    
-                  </Typography>
+                </Typography>
 
-                  <Typography variant='h6' align='center' >
-                    <TextField
-                    error={errors.username && touched.username &&true}
-                      type="text"
-                      name="username"
-                      label='username'
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.username}
-                    />
-                  </Typography>
+                <Typography variant='h6' align='center' >
+                  <Field
+                    component={TextField}
+                    type="text"
+                    name="username"
+                    label='username'
+                  />
+                </Typography>
 
-                  <Typography variant='h6' align='center' >
-                    <TextField
-                    error={errors.email && touched.email && true}
-                      type="email"
-                      name="email"
-                      label='email'
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.email}
-                    />
-                  </Typography>
-                  
-                  <Typography variant='h6' align='center' color='secondary'>
-                    {'Please fill in the fields, all are required'}
-                  </Typography>
+                <Typography variant='h6' align='center' >
+                  <Field
+                    component={TextField}
+                    type="email"
+                    name="email"
+                    label='email'
+                  />
+                </Typography>
 
-                  
-                  <Typography variant='h6' align='center' color='primary'>
-                    {'Rest will be filled with placeholders, You can edit them later.'}
-                  </Typography>
+                <Typography variant='h6' align='center' color='secondary'>
+                  {'Please fill in the fields, all are required'}
+                </Typography>
 
-
-
-                </Grid>
-                <Grid container item xl={11} lg={11} md={11} sm={11} xs={11} justify='space-between'>
-                  <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
-                    <Button onClick={() => history.push('/')} fullWidth variant='outlined' color='primary' >Go Back</Button>
-                  </Grid>
-                  <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
-                    <Button type='submit' disabled={isSubmitting} fullWidth variant='contained' color='primary'>Add User</Button>
-                  </Grid>
-
-                </Grid>
-
-
+                <Typography variant='h6' align='center' color='primary'>
+                  {'Rest will be filled with placeholders, You can edit them later.'}
+                </Typography>
 
               </Grid>
+              <Grid container item xl={11} lg={11} md={11} sm={11} xs={11} justify='space-between'>
+                <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
+                  <Button onClick={() => history.push('/')} fullWidth variant='outlined' color='primary' >Go Back</Button>
+                </Grid>
+                <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
+                  <Button type='submit' disabled={isSubmitting} fullWidth variant='contained' color='primary'>Add User</Button>
+                </Grid>
+                
+              </Grid>
             </Grid>
-          </form>
-        )}
+          </Grid>
+        </Form>
+      )}
     </Formik>
   )
 
@@ -134,7 +110,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = {
-  
+
   requestUserAdd
 };
 
