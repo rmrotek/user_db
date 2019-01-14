a945f70346d87d39cbb9ab84a3ddc1f5c10e5b29
rmrotek <remek87@gmail.com>
2019-01-14 02:23:58 +0100

    adduserform front done, validation done

--- src/components/App/AddUserForm.js
+++ src/components/App/AddUserForm.js
@@ -6,7 +6,7 @@ import { Formik } from 'formik';
 let AddUserForm = ({ history }) => {
   return (
     <Formik
-    initialValues={{ email: '', }}
+      initialValues={{ name: '', username: '', email: '', }}
       validate={values => {
         let errors = {};
         if (!values.email) {
@@ -16,6 +16,13 @@ let AddUserForm = ({ history }) => {
         ) {
           errors.email = 'Invalid email address';
         }
+        if (!values.name) {
+          errors.name = 'Required';
+        }
+        if (!values.username) {
+          errors.username = 'Required';
+        }
+        
         return errors;
       }}
       onSubmit={(values, { setSubmitting }) => {
@@ -25,107 +32,92 @@ let AddUserForm = ({ history }) => {
         }, 400);
       }}
     >
-    {({
-      values,
-      errors,
-      touched,
-      handleChange,
-      handleSubmit,
-      isSubmitting,
-      /* and other goodies */
-    }) => (
-    <form onSubmit={handleSubmit}>
-    <Grid container justify='center'>
-    
-      <Grid container justify='center' item xl={11} lg={11} md={11} sm={11} xs={11} spacing={8} style={{ background: 'lightgrey' }}>
-        <Grid item xl={11} lg={11} md={11} sm={11} xs={11}>
-          <Typography variant='h4' align='center' >
-            Add a User to Database
-              </Typography>
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+        handleBlur
+      }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid container justify='center'>
 
-        </Grid>
-        <Grid item xl={6} lg={6} md={6} sm={11} xs={11} >
+              <Grid container justify='center' item xl={11} lg={11} md={11} sm={11} xs={11} spacing={8} style={{ background: 'lightgrey' }}>
+                <Grid item xl={11} lg={11} md={11} sm={11} xs={11}>
+                  <Typography variant='h4' align='center' >
+                    {'Add a User to Database'}
+                  </Typography>
 
-          <Typography variant='h5' >
-            Personal Data
-              </Typography>
-          <Typography variant='h6' >
-            name:
-              </Typography>
-          <Typography variant='h6' >
-            username:
-              </Typography>
-          <Typography variant='h6' >
-          <TextField
-            type="email"
-            name="email"
-            label='email'
-            onChange={handleChange}
-            value={values.email}
-          />
-          {errors.email && touched.email && errors.email}
-              </Typography>
-          <Typography variant='h6' >
-            phone:
-              </Typography>
-          <Typography variant='h6' >
-            website:
-              </Typography>
+                </Grid>
+                <Grid item xl={6} lg={12} md={12} sm={11} xs={11} >
 
-        </Grid>
+                  <Typography variant='h6' align='center' >
+                    <TextField
+                      error={errors.name && touched.name && true}
+                      type="text"
+                      name="name"
+                      label='name'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}
+                    />
+                    
+                  </Typography>
 
-        <Grid item xl={5} lg={5} md={5} sm={11} xs={11}>
+                  <Typography variant='h6' align='center' >
+                    <TextField
+                    error={errors.username && touched.username &&true}
+                      type="text"
+                      name="username"
+                      label='username'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.username}
+                    />
+                  </Typography>
 
-          <Typography variant='h5' >
-            Address
-              </Typography>
-          <Typography variant='h6' >
-            street:
-              </Typography>
-          <Typography variant='h6' >
-            suite:
-              </Typography>
-          <Typography variant='h6' >
-            city:
-              </Typography>
-          <Typography variant='h6' >
-            zipcode:
-              </Typography>
+                  <Typography variant='h6' align='center' >
+                    <TextField
+                    error={errors.email && touched.email && true}
+                      type="email"
+                      name="email"
+                      label='email'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                    />
+                  </Typography>
+                  
+                  <Typography variant='h6' align='center' color='secondary'>
+                    {'Please fill in the fields, all are required'}
+                  </Typography>
 
-        </Grid>
-
-        <Grid item xl={11} lg={11} md={11} sm={11} xs={11}>
-
-          <Typography variant='h5' >
-            Company
-              </Typography>
-          <Typography variant='h6' >
-            name:
-              </Typography>
-          <Typography variant='h6' >
-            catchPhrase:
-              </Typography>
-          <Typography variant='h6' >
-            bs:
-              </Typography>
-
-        </Grid>
-        <Grid container item xl={11} lg={11} md={11} sm={11} xs={11} justify='space-between'>
-          <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
-            <Button onClick={() => history.push('/')} fullWidth variant='outlined' color='primary' >Go Back</Button>
-          </Grid>
-          <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
-            <Button type='submit' fullWidth variant='contained' color='primary'>Add User</Button>
-          </Grid>
-
-        </Grid>
+                  
+                  <Typography variant='h6' align='center' color='primary'>
+                    {'Rest will be filled with placeholders, You can edit them later.'}
+                  </Typography>
 
 
 
-      </Grid>
-    </Grid>
-    </form>
-)}
+                </Grid>
+                <Grid container item xl={11} lg={11} md={11} sm={11} xs={11} justify='space-between'>
+                  <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
+                    <Button onClick={() => history.push('/')} fullWidth variant='outlined' color='primary' >Go Back</Button>
+                  </Grid>
+                  <Grid item xl={4} lg={4} md={4} sm={12} xs={12}>
+                    <Button type='submit' disabled={isSubmitting} fullWidth variant='contained' color='primary'>Add User</Button>
+                  </Grid>
+
+                </Grid>
+
+
+
+              </Grid>
+            </Grid>
+          </form>
+        )}
     </Formik>
   )
 
