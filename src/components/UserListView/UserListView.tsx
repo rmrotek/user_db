f033f74cab27209913ada7cc1c9e6dd0e9628140
rmrotek <remek87@gmail.com>
2019-01-16 20:01:01 +0100

    userlistview mini style fix

--- src/components/UserListView/UserListView.tsx
+++ src/components/UserListView/UserListView.tsx
@@ -14,7 +14,7 @@ let UserListView = ({}: TProps) => {
   return (
     
     <Grid container justify='center'  >
-      <Grid container item xl={4} lg={4} md={6} sm={12} xs={12}  justify='center'  style={{boxShadow: '0px 0px 5px black'}}>
+      <Grid container item xl={4} lg={4} md={6} sm={6} xs={12}  justify='center'  style={{boxShadow: '0px 0px 5px black'}}>
       <Grid item xl={12} lg={12} md={12} sm={12} xs={12} >
       <Typography variant='h4' align='center' paragraph></Typography>
 
